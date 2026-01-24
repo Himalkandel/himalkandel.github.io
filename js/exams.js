64d16b4050a793ed4c73f9417efd8f14b8ec4bc8
itemblox <55263360+Himalkandel@users.noreply.github.com>
2026-01-23 19:35:55 -0700

    e

--- js/exams.js
+++ js/exams.js
@@ -8,6 +8,7 @@ let examKey = "";
 ========================= */
 function loadExam(file) {
   examKey = `longExamSession_${file}`;
+  localStorage.removeItem(examKey); // 🔥 clears old sessions on phone too
 
   fetch(`../data/exams/${file}`)
     .then(res => res.json())
